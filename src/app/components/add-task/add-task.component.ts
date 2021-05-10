a0f262ef50bf6db0a1147984d329fc5e659f7520
“Haorong <li000592@algonquinlive.com>
2021-05-10 00:25:19 -0400

    :new: added submit & create task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   text: string;
   day: string;
   reminder: boolean = false;
@@ -13,4 +15,22 @@ export class AddTaskComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+    this.onAddTask.emit(newTask);
+    // @todo - emit event
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
